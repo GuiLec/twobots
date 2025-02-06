6009a2e8710522a5a8c410193cd5444acb5bd07c
GuiLec <guillaumele@bam.tech>
2025-02-06 11:53:07 +0100

    set up instructions

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -4,13 +4,21 @@ import { NextResponse } from "next/server";
 const apiKey = process.env.GEMINI_SECRET_KEY;
 const genAI = new GoogleGenerativeAI(apiKey ?? "");
 
+const instructions = "Always start your answers by Yo Yo";
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+  generationConfig: {
+    maxOutputTokens: 100,
+  },
+  systemInstruction: instructions,
+});
+
 export async function POST(request: Request) {
   const body = await request.json();
   const { prompt } = body;
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
     const data = await model.generateContent(prompt);
     const candidates = data.response.candidates ?? [];
     const result = candidates[0].content.parts[0].text;
